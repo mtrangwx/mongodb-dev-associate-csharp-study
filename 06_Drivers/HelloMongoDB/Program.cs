40caa0a0ed0bf61f821935f6886200beaf1ef7f7
Michael Trang <mtrang@woolworths.com.au>
2025-02-15 22:11:15 +1100

    added sample app

---
+++ 06_Drivers/HelloMongoDB/Program.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+var mongoUrl = new MongoUrl("mongodb+srv://myAtlasDBUser:myatlas-001@myatlasclusteredu.akxkn.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU");
+var client = new MongoClient(mongoUrl);
+
+var dbList = client.ListDatabaseNames().ToList();
+// var database = client.GetDatabase("sample_training");
+
+Console.WriteLine("The list of databases are:");
+foreach (var item in dbList)
+{
+    Console.WriteLine(item);
+}
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
